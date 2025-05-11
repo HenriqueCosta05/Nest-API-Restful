19345c578877ca8474289209a6706f88ab1eaebf
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-05-11 07:20:02 -0300

    feat: healthcheck endpoint added

--- src/infra/framework/app.module.ts
+++ src/infra/framework/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions/transactions.controller';
 import { StatisticsController } from './statistics/statistics.controller';
 import { TransactionsModule } from './transactions/transactions.module';
+import { StatisticsModule } from './statistics/statistics.module';
 import { Logger } from 'winston';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [TransactionsModule],
-    controllers: [TransactionsController, StatisticsController],
+    imports: [ TransactionsModule, StatisticsModule, TerminusModule, HealthModule ],
+    controllers: [TransactionsController, StatisticsController, HealthController],
     providers: [Logger],
 })
 export class AppModule { }
