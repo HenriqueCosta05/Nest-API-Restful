25833878aea5162e1ba72ff1a21bb4a2e719e36f
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-05-12 21:16:45 -0300

    bugfix: removed ref forbidNonWhitelisted

--- src/infra/framework/main.ts
+++ src/infra/framework/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
         new ValidationPipe({
             whitelist: true,
             transform: true,
-            forbidNonWhitelisted: true,
             transformOptions: {
                 enableImplicitConversion: true,
             },
